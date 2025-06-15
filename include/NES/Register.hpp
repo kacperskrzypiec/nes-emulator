4264e8dd1950d4c59aeaed26ae7cb32cf4ddb2f1
kacperskrzypiec <212605675+kacperskrzypiec@users.noreply.github.com>
2025-06-15 14:52:21 +0200

    Overloaded [] operator for Register

--- include/NES/Register.hpp
+++ include/NES/Register.hpp
@@ -8,13 +8,11 @@ namespace ks {
 		uint8_t data{};
 
 		auto get_bit(const uint8_t index) const -> bool {
-			assert(index >= 0 && "Index should be non-negative.");
 			assert(index < 8 && "Index should be less than 8.");
 
 			return (data >> index) & 0x1;
 		}
 		auto set_bit(const uint8_t index, const bool value) -> void {
-			assert(index >= 0 && "Index should be non-negative.");
 			assert(index < 8 && "Index should be less than 8.");
 
 			data ^= (-(int8_t)value ^ data) & (1 << index);
@@ -34,6 +32,34 @@ namespace ks {
 
 		operator uint8_t() const {
 			return data;
+		}
+		auto operator[](const uint8_t index) const -> bool {
+			assert(index < 8 && "Index should be less than 8.");
+			return get_bit(index);
+		}
+
+		class RegRef {
+		public:
+			RegRef(Register& reg, const uint8_t index)
+				:	m_reg(reg), m_index(index) {}
+
+			auto operator=(const bool value) -> RegRef& {
+				m_reg.set_bit(m_index, value);
+				return *this;
+			}
+
+			operator bool() const {
+				return m_reg.get_bit(m_index);
+			}
+
+		private:
+			Register& m_reg;
+			uint8_t m_index;
+		};
+
+		auto operator[](const uint8_t index) -> RegRef {
+			assert(index < 8 && "Index should be less than 8.");
+			return RegRef(*this, index);
 		}
 	};
 }
--- test/Register_test.cpp
+++ test/Register_test.cpp
@@ -9,33 +9,33 @@ TEST_CASE("Register behavior")
 	FAST_CHECK_EQ(reg, 0);
 
 	// Test setting bits
-	reg.set_bit(4, 1);
+	reg[4] = 1;
 	FAST_CHECK_EQ(reg, 0x10);
-	FAST_CHECK_EQ(reg.get_bit(4), 1);
+	FAST_CHECK_EQ(reg[4], 1);
 
-	reg.set_bit(7, 1);
+	reg[7] = 1;
 	FAST_CHECK_EQ(reg, 0x90);
-	FAST_CHECK_EQ(reg.get_bit(4), 1);
-	FAST_CHECK_EQ(reg.get_bit(7), 1);
+	FAST_CHECK_EQ(reg[4], 1);
+	FAST_CHECK_EQ(reg[7], 1);
 
-	reg.set_bit(0, 1);
+	reg[0] = 1;
 	FAST_CHECK_EQ(reg, 0x91);
-	FAST_CHECK_EQ(reg.get_bit(0), 1);
-	FAST_CHECK_EQ(reg.get_bit(4), 1);
-	FAST_CHECK_EQ(reg.get_bit(7), 1);
+	FAST_CHECK_EQ(reg[0], 1);
+	FAST_CHECK_EQ(reg[4], 1);
+	FAST_CHECK_EQ(reg[7], 1);
 
 	// Test setting bits to 0
 	reg = {};
 	FAST_CHECK_EQ(reg, 0);
 	for (int i = 0; i < 8; ++i) {
-		FAST_CHECK_EQ(reg.get_bit(i), 0);
+		FAST_CHECK_EQ(reg[i], 0);
 	}
 
 	// Test setting bits one by one
 	for(int i = 0; i < 8; ++i) {
-		reg.set_bit(i, 1);
+		reg[i] = 1;
 		for (int j = 0; j < i; j++) {
-			FAST_CHECK_EQ(reg.get_bit(j), 1);
+			FAST_CHECK_EQ(reg[j], 1);
 		}
 	}
 	
